ae6eedbfd668d6d40aa294fa94126af88381d29a
Nadia Escobar <escobarnad@gmail.com>
2024-03-21 15:31:19 +0100

    Iteration 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,47 @@
-function AllBeersPage() {}
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState();
+
+  const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
+  const fetchAllBeers = async () => {
+    const response = await fetch(`${API_URL}`);
+    if (response.ok) {
+      const allBeers = await response.json();
+      setBeers(allBeers);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllBeers();
+  }, []);
+
+  if (!beers) {
+    return <p>Loading...</p>;
+  }
+  console.log(beers);
+
+  return (
+    <div>
+      {beers &&
+        beers.map((beer) => {
+          return (
+            <Link to={`/beers/${beer._id}`} key={beer._id}>
+              <img
+                src={beer.image_url}
+                alt={`${beer.name} image`}
+                style={{ height: "10rem" }}
+              />
+              <h2>{beer.name}</h2>
+              <h3>{beer.tagline}</h3>
+              <p>{beer.contributed_by}</p>
+            </Link>
+          );
+        })}
+    </div>
+  );
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,49 +1,56 @@
 import beers from "../assets/beers.png";
 import randomBeer from "../assets/random-beer.png";
 import newBeer from "../assets/new-beer.png";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <>
-      <img src={beers} alt="All Beers Image" />
-      <h1>All Beers</h1>
-      <span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </span>
-      <img src={randomBeer} alt="Random Beer Image" />
-      <h1>Random Beer</h1>
-      <span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </span>
-      <img src={newBeer} alt="New Beer Image" />
-      <h1>New Beer</h1>
-      <span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </span>
+      <Link to={"/beers"}>
+        <img src={beers} alt="All Beers Image" />
+        <h1>All Beers</h1>
+        <span>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+      </Link>
+      <Link to={"/random-beer"}>
+        <img src={randomBeer} alt="Random Beer Image" />
+        <h1>Random Beer</h1>
+        <span>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+      </Link>
+      <Link to={"/new-beer"}>
+        <img src={newBeer} alt="New Beer Image" />
+        <h1>New Beer</h1>
+        <span>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+      </Link>
     </>
   );
 }
