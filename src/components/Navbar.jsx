3f2b6485fa5a5380c019dcfd15f35877799a6956
Nadia Escobar <escobarnad@gmail.com>
2024-03-21 15:07:04 +0100

    Iteration 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,22 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import homeIcon from "../assets/home-icon.png";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        background: "#3281a8",
+        padding: "1rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link to="/">
+        <img src={homeIcon} alt="Home icon" style={{ height: "2rem" }} />
+      </Link>
+    </nav>
+  );
+}
 
 export default Navbar;
